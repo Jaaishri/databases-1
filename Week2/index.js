853ef7a2c7efbe290e9ee7daf5d2069aee0c4cb6
Jayshree Upadhyay <51030227+Jaaishri@users.noreply.github.com>
2019-12-05 00:08:20 +0100

    Update index.js

--- Week2/index.js
+++ Week2/index.js
@@ -36,7 +36,7 @@ async function queryDb() {
     const chooseFirstQuery = `SELECT name from city where id in (select capital from country where name = ?)`;
     const chooseSecondQuery = `SELECT countrylanguage.language ,country.region FROM country JOIN countrylanguage ON countrylanguage.CountryCode = country.code WHERE country.region = ?;`;
     const chooseThirdQuery = `SELECT COUNT(city.Name) AS total FROM countrylanguage JOIN city ON city.CountryCode = countrylanguage.CountryCode WHERE countrylanguage.language = ?`;
-    const chooseForthQuery = `SELECT continent, COUNT(DISTINCT language) AS numberOflanguages FROM country LEFT JOIN countrylanguage ON country.code = countrylanguage.countryCode GROUP BY continent`;
+    const chooseFourthQuery = `SELECT continent, COUNT(DISTINCT language) AS numberOflanguages FROM country LEFT JOIN countrylanguage ON country.code = countrylanguage.countryCode GROUP BY continent`;
     const chooseFifthQuery = `SELECT country.Name FROM country, countrylanguage WHERE (country.Code = countrylanguage.CountryCode) AND (country.Region= ?) AND (countrylanguage.Language= ?) AND IsOfficial = 'T'`;
 
     switch (userMakeChoices) {
@@ -62,8 +62,8 @@ async function queryDb() {
         break;
 
       case "4":
-        const responseForth = await execQuery(chooseForthQuery);
-        console.log(responseForth);
+        const responseFourth = await execQuery(chooseFourthQuery);
+        console.log(responseFourth);
         break;
 
       case "5":
