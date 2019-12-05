31457a3f9311130349526319720f50f3dad2b990
Jayshree Upadhyay <51030227+Jaaishri@users.noreply.github.com>
2019-12-05 17:42:04 +0100

    TRIGGER created

---
+++ Week2/part3.sql
@@ -0,0 +1,17 @@
+delimiter //
+
+CREATE TRIGGER language_Max_Trigger BEFORE
+INSERT ON countrylanguage FOR EACH ROW BEGIN
+DECLARE message varchar (250);
+DECLARE languages INT;
+SET languages =
+(SELECT COUNT(language)
+FROM countrylanguage
+WHERE countrycode = NEW.countrycode);
+IF languages > 9 THEN
+set message = 'More than 9 languages are spoken in the selected country';
+SET lc_messages = message; SIGNAL SQLSTATE '45000';
+END
+IF;
+END //
+delimiter ;
